fc2de8dc4beac948108bd2cbe93794f88c54dc32
Kimado <Kimado@KIMADO-PC>
2016-04-11 14:36:15 -0400

    Finished the hook function with pID and Read Permission.

--- src/Serenade/Console Methods/setConsole.cs
+++ src/Serenade/Console Methods/setConsole.cs
@@ -5,26 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
 
-/* Warning, my code structure suck so you'll get lost. have fun... */
-/* @Kimado 
-
-░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
-░░░░░░░▄▄▀▀▀▀▀▀▀▀▀▀▄▄█▄░░░░▄░░░░█░░░░░░░
-░░░░░░█▀░░░░░░░░░░░░░▀▀█▄░░░▀░░░░░░░░░▄░
-░░░░▄▀░░░░░░░░░░░░░░░░░▀██░░░▄▀▀▀▄▄░░▀░░
-░░▄█▀▄█▀▀▀▀▄░░░░░░▄▀▀█▄░▀█▄░░█▄░░░▀█░░░░
-░▄█░▄▀░░▄▄▄░█░░░▄▀▄█▄░▀█░░█▄░░▀█░░░░█░░░
-▄█░░█░░░▀▀▀░█░░▄█░▀▀▀░░█░░░█▄░░█░░░░█░░░
-██░░░▀▄░░░▄█▀░░░▀▄▄▄▄▄█▀░░░▀█░░█▄░░░█░░░
-██░░░░░▀▀▀░░░░░░░░░░░░░░░░░░█░▄█░░░░█░░░
-██░░░░░░░░░░░░░░░░░░░░░█░░░░██▀░░░░█▄░░░
-██░░░░░░░░░░░░░░░░░░░░░█░░░░█░░░░░░░▀▀█▄
-██░░░░░░░░░░░░░░░░░░░░█░░░░░█░░░░░░░▄▄██
-░██░░░░░░░░░░░░░░░░░░▄▀░░░░░█░░░░░░░▀▀█▄
-░▀█░░░░░░█░░░░░░░░░▄█▀░░░░░░█░░░░░░░▄▄██
-░▄██▄░░░░░▀▀▀▄▄▄▄▀▀░░░░░░░░░█░░░░░░░▀▀█▄
-░░▀▀▀▀░░░░░░░░░░░░░░░░░░░░░░█▄▄▄▄▄▄▄▄▄██
-░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ */
 
 // Changed the console folder to ConsoleMethods\ so we know where stuff are.
 namespace Serenade.ConsoleMethods
@@ -45,37 +25,6 @@ namespace Serenade.ConsoleMethods
             Console.ForegroundColor = color;
             Console.Write(message);
             Console.ResetColor();
-        }
-
-        // I wrote a simple loop /-\| for the hook function and I ended up putting it in the setConsole class.
-        public static void hookloop()
-        {
-            // Initialized waittime to 34 so it doesn't "lag".
-            int waittime = 34;
-            
-            // Writing default Time & Message before enabling the loop because I removed it from the Write function by default. 
-            Write(ConsoleColor.Yellow, "[ " + DateTime.Now + "] - " +  "Waiting for call of duty 4... ");
-
-            // Ok Uh, it a simple FindWindow searching method combined with a console loop. Nothing really complex. 
-            while ((HookMethods.Hook.GameWindowHandle = Calling_Native_Methods.NativeMethods.FindWindow(null, "Call of Duty 4")) == IntPtr.Zero)
-            {
-                /* Kinda pointless to comment dis no? */ 
-                Write(ConsoleColor.Yellow, "/");
-                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
-                System.Threading.Thread.Sleep(waittime);
-                Write(ConsoleColor.Yellow, "-");
-                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
-                System.Threading.Thread.Sleep(waittime);
-                Write(ConsoleColor.Yellow, "\\");
-                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
-                System.Threading.Thread.Sleep(waittime);
-                Write(ConsoleColor.Yellow, "|");
-                Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
-                System.Threading.Thread.Sleep(waittime);
-            }
-            /* The setCursorPosition used for the loop fuck up a bit the line so we created a new blackline empty space before writing more text. */
-            WriteLine(ConsoleColor.Black, " ");
-            WriteLine(ConsoleColor.Green, "\"Call of duty 4\" window initialized... ");
         }
     }
 }
--- src/Serenade/Hook Methods/Hook.cs
+++ src/Serenade/Hook Methods/Hook.cs
@@ -12,11 +12,45 @@ namespace Serenade.HookMethods
         public static IntPtr GameProcessHandle;
         public static void HookWindow()
         {
-            // Simple Hook Functions...
-            ConsoleMethods.setConsole.hookloop();
+            // Initialized waittime to 34 so it doesn't "lag".
+            int waittime = 34;
 
-            // Next step will be to get the process ID from the game and a read permissions.
-            // Wait me before starting (: 
+            // Writing default Time & Message before enabling the loop because I removed it from the Write function by default. 
+            ConsoleMethods.setConsole.Write(ConsoleColor.Yellow, "[ " + DateTime.Now + "] - " + "Waiting for call of duty 4... ");
+
+            // Ok Uh, it a simple FindWindow searching method combined with a console loop. Nothing really complex. 
+            while ((HookMethods.Hook.GameWindowHandle = Calling_Native_Methods.NativeMethods.FindWindow(null, "Call of Duty 4")) == IntPtr.Zero)
+            {
+                // Kinda pointless to comment dis no? 
+                ConsoleMethods.setConsole.Write(ConsoleColor.Yellow, "/"); Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop); System.Threading.Thread.Sleep(waittime); ConsoleMethods.setConsole.Write(ConsoleColor.Yellow, "-"); Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop); System.Threading.Thread.Sleep(waittime); ConsoleMethods.setConsole.Write(ConsoleColor.Yellow, "\\"); Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop); System.Threading.Thread.Sleep(waittime); ConsoleMethods.setConsole.Write(ConsoleColor.Yellow, "|"); Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop); System.Threading.Thread.Sleep(waittime);
+            }
+            // The setCursorPosition used for the loop fuck up a bit the line so we created a new black line for an empty space before writing more text. 
+            ConsoleMethods.setConsole.WriteLine(ConsoleColor.Black, " ");
+            ConsoleMethods.setConsole.WriteLine(ConsoleColor.Green, "\"Call of duty 4\" Window initialized... ");
+
+            // Getting the Process ID from Call of Duty 4 window. 
+            int pID;
+            Calling_Native_Methods.NativeMethods.GetWindowThreadProcessId(GameWindowHandle, out pID);
+           
+            // Now we got our Process ID, write it in the logs console.
+            if(pID == 0)
+                ConsoleMethods.setConsole.WriteLine(ConsoleColor.Red, "Ooops!? Couldn't get the process id! ");
+            else
+                ConsoleMethods.setConsole.WriteLine(ConsoleColor.Green, "\"Call of duty 4\" Process id: " + pID + "...");
+
+            // Handle the Process and Get Read Permissions.
+            // public static extern IntPtr OpenProcess(uint dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, int dwProcessId);
+            GameProcessHandle = Calling_Native_Methods.NativeMethods.OpenProcess(Calling_Native_Methods.NativeMethods.PROCESS_VM_READ, false, pID);
+
+            // Handle the Process and Get Read Permissions.
+            if (GameProcessHandle == IntPtr.Zero) {
+                ConsoleMethods.setConsole.WriteLine(ConsoleColor.Red, "Ooops!? Couldn't recieve read permission of the process: " + pID + "!");
+                ConsoleMethods.setConsole.WriteLine(ConsoleColor.Yellow, "Please, make sure that you aren't running your game in administrator mode...");
+            }
+                
+            else
+                ConsoleMethods.setConsole.WriteLine(ConsoleColor.Green, "\"Call of duty 4\" Read permission: True...");
+
         }
     }
 }
